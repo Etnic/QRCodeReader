7cfe8dfa26d625ff646f9c8d09ae95d2edef5c66
Martin Kubus <martin.kubus@disig.sk>
2019-11-15 08:29:50 +0100

    Add project files.

---
+++ QRCodeReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QRCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
